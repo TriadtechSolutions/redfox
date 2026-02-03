2d8a359c38afc559097787e0a6572dae1a5d3c51
Muthukumar s <muthu.triadtechsolutions@gmail.com>
2026-02-04 00:25:08 +0530

    Commerce updates

--- web/themes/custom/redfox/js/global.js
+++ web/themes/custom/redfox/js/global.js
@@ -3,14 +3,60 @@
  * Global utilities.
  *
  */
-(function (Drupal) {
+(function (Drupal,once) {
 
   'use strict';
 
   Drupal.behaviors.redfox = {
     attach: function (context, settings) {
 
+      const galleries = once(
+        'image-gallery',
+        '.main-image-container',
+        context
+      );
+
+      galleries.forEach((mainContainer) => {
+
+        const thumbnailContainer = context.querySelector('.thumbnail-gallery');
+        if (!thumbnailContainer) {
+          return;
+        }
+
+        const allImages = thumbnailContainer.querySelectorAll('.field__item');
+        if (!allImages.length) {
+          return;
+        }
+
+        // Show first image in main container
+        mainContainer.innerHTML = '';
+        const firstImageClone = allImages[0].cloneNode(true);
+        mainContainer.appendChild(firstImageClone);
+
+        // Setup thumbnails
+        allImages.forEach((thumbnail, index) => {
+          thumbnail.classList.add('thumbnail-item');
+
+          if (index === 0) {
+            thumbnail.classList.add('active');
+          }
+
+          thumbnail.addEventListener('click', (e) => {
+            e.preventDefault();
+
+            allImages.forEach(t => t.classList.remove('active'));
+            thumbnail.classList.add('active');
+
+            mainContainer.innerHTML = '';
+            const imageClone = thumbnail.cloneNode(true);
+            imageClone.classList.remove('thumbnail-item', 'active');
+            mainContainer.appendChild(imageClone);
+          });
+        });
+
+      });
+
     }
   };
 
-})(Drupal);
+})(Drupal,once);
